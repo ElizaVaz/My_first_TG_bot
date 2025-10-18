ffcbdb1a8f161028e7d947c84750f5cd80e8f0e4
ElizaVaz <150373929+ElizaVaz@users.noreply.github.com>
2025-10-18 10:46:51 +0300

    Update main.py
    Код кодирующего бота, который РАБОТАЕТ!!!

--- main.py
+++ main.py
@@ -17,17 +17,30 @@ FLAG = False
 # Handle '/start'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
+   bot.reply_to(message, """\
 Привет, я ЦезарьБот.
 Я здесь, чтобы зашифровать или расшифровать ваше сообщение. Просто напишите что-нибудь и я зашифрую ваше сообщение!\
 """)
+
+# @bot.message_handler(commands=['start'])
+# def welcome(message):
+#     chat_id = message.chat.id
+#     keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     button_support = telebot.types.KeyboardButton(text="Выберете команду")
+#     button1 = telebot.types.KeyboaoardButton(text="/encode")
+#     button3 = telebot.types.KeyboardButton(text="/decode")
+#     keyboard.add(buttrdButton(text="/help")
+#     button2 = telebot.types.Keybon_support, button1, button2, button3)
+#     bot.send_message(chat_id,
+#                      'Добро пожаловать в бота кодировщика!',
+#                      reply_markup=keyboard)
 
 
 # Handle '/help'
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
     bot.reply_to(message, """\
-- напиши текст и я его зашифрую;\n- напиши зашифрованный текст и его расшифрую.\
+/encode - напиши текст и я его зашифрую;\n/decode- напиши зашифрованный текст и его расшифрую.\
 """)
 
 
@@ -44,21 +57,19 @@ def com_encode(message):
 def encoded_message(message):
     global FLAG
     if FLAG:
-        list_message = list(message.text)
+        list_message = list(message.text.lower())
         message1 = ""
         for x in list_message:
             if x in r_letters:
                 message1 += r_letters[int(r_letters.index(x) + 3) % (len(r_letters) - 1)]
+            elif x in e_letters:
+                message1 += e_letters[int(e_letters.index(x) + 3) % (len(e_letters) - 1)]
             else:
                 message1 += x
-            
-#       elif list_message[0] in e_letters:
-#           list_message = [e_letters[int(e_letters.index(x) + 3) % (len(e_letters) - 1)] if x in list_message else x for x in list_message]
-    
-        bot.reply_to(message, message1.text)
+        bot.reply_to(message, message1)
         FLAG = False
     else:
-        bot.reply_to(message, "ДабудтДабудай, вы просто вводите текст")
+        bot.reply_to(message, "Вы просто вводите текст, если хотите закодировать или декодировать текст используйте функции. Подробнее ищите в /help.")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
